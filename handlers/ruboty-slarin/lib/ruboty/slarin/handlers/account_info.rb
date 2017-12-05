a899d908fe4bdec5a5df58b6632e72cafb95ea8f
SHIOYA, Hiromu <kwappa.856@gmail.com>
2017-12-05 18:08:35 +0900

    :lock: Reply at only arrowd channels

--- handlers/ruboty-slarin/lib/ruboty/slarin/handlers/account_info.rb
+++ handlers/ruboty-slarin/lib/ruboty/slarin/handlers/account_info.rb
@@ -6,6 +6,8 @@ module Ruboty
         on(/my_account/, name: 'my_account',description: 'show your account info')
 
         def account_info(message)
+          return unless arrowed_channel?(message)
+
           client = Ruboty::Slarin::SlackClient.new
           users  = client.find_users_by_emails(message[:emails])
 
@@ -20,6 +22,11 @@ module Ruboty
         def my_account(message)
           client = Ruboty::Slarin::SlackClient.new
           client.send_account_info(message)
+        end
+
+        def arrowed_channel?(message)
+          arrowed_channel = ENV['SLARIN_GUEST_ROOMS'].chomp.split(',')
+          arrowed_channel.include?(message.original[:from])
         end
       end
     end
